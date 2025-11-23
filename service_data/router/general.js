8034376bc111fb1de8726cb13392b0988fef4e44
sergei <ser.niev@gmail.com>
2025-11-23 13:41:00 -0500

    ADD: add code to get infromation about list of books

--- service_data/router/general.js
+++ service_data/router/general.js
@@ -11,27 +11,46 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn', function (req, res) {
+  const number = req.params.isbn;
+  if (books[number]) {
+    return res.status(200).send(books[number]);
+  } else {
+    return res.status(410).json({message: "Error: No book found for given isbn"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const selectedAuthor = req.params.author;
+  let foundBooks = [];
+
+  const keys = Object.keys(books);
+  for (element in keys) {
+    if (books[element].author === selectedAuthor) {
+        foundBooks.push(books[element]);
+    }
+  }
+  return res.status(200).json(foundBooks);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const selectedTitle = req.params.title;
+  let foundBooks = [];
+
+  const keys = Object.keys(books);
+  for (element in keys) {
+    if (books[element].title === selectedTitle) {
+        foundBooks.push(books[element]);
+    }
+  }
+  return res.status(200).json(foundBooks);
 });
 
 //  Get book review
